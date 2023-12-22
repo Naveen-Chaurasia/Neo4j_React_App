ed66fab7d29779ab32519de793b59cca25ec68c6
Naveen <naveen.chaurasia861@gmail.com>
2023-12-22 21:49:29 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,59 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+var neo4j = require('neo4j-driver');
+
+const URI = 'neo4j+s://ac4ba3f7.databases.neo4j.io'
+const USER = 'neo4j'
+const PASSWORD = 'AWKEeX3JP4VQaD5ch-O6Z_2kuu6ksfJWCUCwdIFJHR0'
+
+function HelloWorld() {
+  
+  return <h1 className="greeting">Hello, world!</h1>;
+
+}
+
+(async () => {
+  var neo4j = require('neo4j-driver')
+
+  let driver
+
+  try {
+    driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
+    const serverInfo = await driver.getServerInfo()
+    console.log('Connection estabilished')
+    console.log(serverInfo)
+  } catch(err) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+    await driver.close()
+    return
+  }
+  const { records, summary, keys } = await driver.executeQuery(
+    'MATCH (n) RETURN n.name AS name',
+    { age: 42 },
+    { database: 'neo4j' }
+  )
+  
+  // Summary information
+  console.log(
+    `>> The query ${summary.query.text} ` +
+    `returned ${records.length} records ` +
+    `in ${summary.resultAvailableAfter} ms.`
+  )
+
+  // Use the driver to run queries
+
+  await driver.close()
+
+
+})();
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
